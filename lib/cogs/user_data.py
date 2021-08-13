bb65ca8e4e63223f4d36ace00fbc0b5906d8a756
Matthew Zhang <70178663+caspinprince@users.noreply.github.com>
2021-08-13 10:48:59 -0400

    improved stats command, added reset command

--- lib/cogs/user_data.py
+++ lib/cogs/user_data.py
@@ -10,7 +10,7 @@ class EmotionCog(commands.Cog, name='User_Data'):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author == self.bot.user:
+        if message.author == self.bot.user or message.content.startswith(('$', ';', '!', '/', '&', '@', '%')):
             return
 
         db = sqlite3.connect('main.sqlite')
@@ -54,12 +54,27 @@ class EmotionCog(commands.Cog, name='User_Data'):
             cursor.execute(f"SELECT joy, sadness, anger, fear, love, surprise FROM emotion_data "
                            f"WHERE guild_id = '{ctx.message.guild.id}' AND user_id = '{ctx.message.author.id}'")
             result = cursor.fetchone()
-            await ctx.send(f"Joy: {str(result[0])}\nSadness: {str(result[1])}\nAnger: {str(result[2])}\n"
+            await ctx.send(f">>> __**Emotion Stats for {ctx.message.author.display_name}**__\n\n"
+                           f"Joy: {str(result[0])}\nSadness: {str(result[1])}\nAnger: {str(result[2])}\n"
                            f"Fear: {str(result[3])}\nLove: {str(result[4])}\nSurprise: {str(result[5])}")
             cursor.close()
             db.close()
 
-
+    @commands.command()
+    async def reset(self, ctx):
+        db = sqlite3.connect('main.sqlite')
+        cursor = db.cursor()
+        sql = (f"UPDATE emotion_data SET joy = ?, sadness = ?, anger = ?, fear = ?, love = ?, surprise = ?"
+               f"WHERE guild_id = ? AND user_id = ?")
+        val = (0, 0, 0, 0, 0, 0, str(ctx.message.guild.id), str(ctx.message.author.id))
+        try:
+            cursor.execute(sql, val)
+            db.commit()
+            await ctx.send(':white_check_mark: User stats were reset!')
+        except Exception as e:
+            await ctx.send(':x: User stats could not be reset!')
+        cursor.close()
+        db.close()
 
 def setup(bot):
     bot.add_cog(EmotionCog(bot))
